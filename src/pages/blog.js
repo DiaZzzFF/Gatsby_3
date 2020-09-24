9f9971b60d28d16e630f19c5d043546e460f4d94
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-09-24 15:46:34 +0300

    Подключает BlogPage

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,50 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
+import React from "react";
+import {graphql} from "gatsby";
 
-const Blog = () => {
-  return <h2>blog page</h2>
-}
+import Layout from "../components/Layout";
+import Blogs from "../components/Blogs";
 
-export default Blog
+
+const BlogPage = ({data}) => {
+  const {
+    allStrapiBlogs: {
+      nodes: blogs
+    }
+  } = data;
+
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs
+          title="blog"
+          blogs={blogs}
+        />
+      </section>
+    </Layout>
+  );
+};
+
+
+export const query = graphql`
+  {
+    allStrapiBlogs(sort: {fields: title, order: DESC}) {
+      nodes {
+        slug
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BlogPage;
