9831ffb31b5591ca3ad3d748ccedf27958b2dd40
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-09-25 17:18:48 +0300

    Подключает ContactPage

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,54 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
 
-const contact = () => {
-  return <h2>contact page</h2>
-}
+import Layout from "../components/Layout";
 
-export default contact
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+
+          <form
+            action="https://formspree.io/xleoeqwg"
+            method="POST"
+          >
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="name"
+                required
+              />
+
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                placeholder="email"
+                required
+              />
+
+              <textarea
+                className="form-control"
+                name="message"
+                placeholder="message"
+                rows="10"
+                required
+              />
+            </div>
+
+            <button className="submit-btn btn" type="submit">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  );
+};
+
+
+export default ContactPage;
