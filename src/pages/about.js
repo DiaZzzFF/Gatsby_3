be7d975aa786a7a22c6803ef253a5eb50859d7f5
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-09-25 14:38:57 +0300

    Подключает AboutPage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,70 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Title from "../components/Title"
-import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
-}
+import React from "react";
+import {graphql} from "gatsby";
+import Image from "gatsby-image";
 
-export default About
+import Layout from "../components/Layout";
+import Title from "../components/Title";
+
+
+const AboutPage = ({data}) => {
+  const {
+    about: {
+      nodes
+    },
+  } = data;
+  const {image, title, info, stack} = nodes[0];
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image
+            className="about-img"
+            fluid={image.childImageSharp.fluid}
+          />
+
+          <article className="about-text">
+            <Title title={title} />
+
+            <p>{info}</p>
+
+            <div className="about-stack">
+              {stack.map((item) => {
+                return (
+                  <span key={item.id}>
+                    {item.title}
+                  </span>
+                );
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+
+export const query = graphql`
+  {
+    about: allStrapiAbout {
+      nodes {
+        image {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+        title
+        info
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default AboutPage;
