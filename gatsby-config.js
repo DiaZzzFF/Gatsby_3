0e9eb3943fa70c8591fe65c5d0012ce81e616688
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-09-26 09:50:13 +0300

    Устанавливает sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
